dd1ef6d937d55694024b7419b7b2054fcbcdcbf7
j891936 <osobajamaldeen35@gmail.com>
2025-03-29 16:01:58 +0100

    final result

--- script.js
+++
@@ -1,8 +0,0 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.classList.add('show')
-}
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.classList.remove('show')
-}
